9a7d9b344e13b0979519e06039542cd778112c51
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-06 10:05:26 +0200

    feat: added link

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
-import UserPage from './pages/Home/UserPage';
+import UserPage from './pages/UserPage/UserPage';
 import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
+import './app.scss';
 
 const App = () => {
 	return (
-		<div>
-			<Navigation />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/user' element={<UserPage />} />
-			</Routes>
-		</div>
+		<BrowserRouter>
+			<div className='app'>
+				<Navigation />
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/user' element={<UserPage />} />
+				</Routes>
+			</div>
+			<Footer />
+		</BrowserRouter>
 	);
 };
 
